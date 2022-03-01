57255a5403492463dd657af991e0a79a5c16d061
Soohan Lee <soohan.m.lee@gmail.com>
2022-03-01 19:28:11 +0900

    add initial project files

---
+++ Messenger/Sources/App/MessengerApp.swift
@@ -0,0 +1,19 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Soohan Lee on 2022/02/28.
+//
+
+import SwiftUI
+
+
+@main
+struct MessengerApp: App {
+  
+  var body: some Scene {
+    WindowGroup {
+      EmptyView()
+    }
+  }
+}
